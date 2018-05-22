6b00a937b429beddc2bf8cfdbe1d55a3a6f355f5
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-22 16:06:05 -0400

    Style sidebar

--- src/NoteList.js
+++ src/NoteList.js
@@ -2,10 +2,63 @@ import React from 'react'
 
 const NoteList = () => {
     return (
-        <div className="NoteList">
-            NoteList
+        <div className="NoteList" style={styles.noteList}>
+            <h3>Notes</h3>
+                <ul id="notes">
+                    <a className="active">
+                        <li>
+                            <div className="note">
+                                <div className="note-title">
+                                     Kohlrabi welsh
+                                </div>
+                                <div className="note-body">
+                                    <p>
+                                        Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                    </a>
+                    <a>
+                         <li>
+                            <div className="note">
+                                <div className="note-title">
+                                    Dandelion cucumber
+                                </div>
+                                <div className="note-body">
+                                    <p>
+                                        Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                    </a>
+                <a>
+                    <li>
+                        <div className="note">
+                            <div className="note-title">
+                                Prairie turnip
+                            </div>
+                            <div className="note-body">
+                                <p>
+                                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                </a>
+            </ul>
         </div>
     )
+}
+
+const styles = {
+    noteList: {
+        borderLeft: '0.1rem solid #eee',
+        borderRight: '0.1rem soloid #eee',
+        width: '30rem',
+    },
+    
 }
 
 export default NoteList
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,21 +6,97 @@ import newHover from './new-hover.png'
 
 const Sidebar = () => {
     return (
-        <nav className="Sidebar">
-            <div className="logo">
-                <img src={quill} alt="Noteherder" />
+        <nav 
+            className="Sidebar"
+            style={styles.sidebar}
+        >
+            <div 
+                className="logo"
+                style={styles.logo}
+            >
+                <img 
+                    src={quill} 
+                    alt="Noteherder" 
+                    style={styles.logoImg}
+                />
             </div>
-            <a className="new-note" href="/notes">
-                <img src={newHover} alt="New note" />
-                <img className="outline" src={newIcon} alt="New note" />
+            <a 
+                className="new-note" 
+                href="/notes" 
+                style={styles.newNote}
+            >
+                <img 
+                    style={styles.aImg} 
+                    src={newHover} 
+                    alt="New note" 
+                />
+                <img 
+                    className="outline" 
+                    style={styles.hover} 
+                    src={newIcon} 
+                    alt="New note" 
+                />
             </a>
-            <div className="SignOut">
-                <button>
-                    <i className="fa fa-sign-out"></i>
+            <div 
+                className="SignOut"
+                style={styles.signOut}
+            >
+                <button style={styles.signOutB}>
+                    <i className="fa fa-sign-out" style={styles.signOutBI}></i>
                 </button>
             </div>
         </nav>
     )
+}
+
+const styles = {
+    sidebar: {
+        width: '6rem',
+        backgroundColor: '#f3f3f3',
+        padding: '0.5rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    logo: {
+        fontFamily: '"Fauna One"',
+        color: '#666',
+        fontSize: '3rem',
+    },
+    logoImg: {
+        width: '3rem',
+        paddingLeft: '0.4rem',
+    },
+    newNote: {
+        marginTop: '2rem',
+        position: 'relative',
+        width: '4rem',
+    },
+    button: {
+        backgroundColor: 'transparent',
+        border: '0',
+        color: '#008BF8',
+        cursor: 'pointer',
+    },
+    aImg: {
+        position: 'absolute',
+        left: '0',
+        width: '100%',
+        transition: 'opacity 0.25s ease-in-out',
+    },
+    hover: {
+        opacity: '0',
+    },
+    signOut: {
+        position: 'absolute',
+        bottom: '1rem',
+    },
+    signOutB: {
+        outline: 'none',
+    },
+    signOutBI: {
+        fontSize: '2rem',
+    },
 }
 
 export default Sidebar
