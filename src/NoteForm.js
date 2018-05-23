a1df4a70196a810e72e3885805a86139ac34cd13
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-23 15:24:49 -0400

    Use destructoring assignment on props

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './NoteForm.css'
 
-const NoteForm = (props) => {
+const NoteForm = ({ notes, currentNote }) => {
     return (
         <div className="NoteForm">
           <div className="form-actions">
@@ -15,10 +15,10 @@ const NoteForm = (props) => {
                 type="text"
                 name="title"
                 placeholder="Title your note"
-                value={props.currentNote.title}
+                value={currentNote.title}
               />
             </p>
-            <textarea name="body" value={props.currentNote.body}></textarea>
+            <textarea name="body" value={currentNote.body}></textarea>
           </form>
         </div>
     )
