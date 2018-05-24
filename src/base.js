f28d42f60006a99f49a3860e3ed691eab86ff404
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-24 14:34:38 -0400

    Add firebase realtime database

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAWyFQXkzhol-mugZ7Vhn2zHVv9kwYiXXc",
+    authDomain: "noteherder-e7579.firebaseapp.com",
+    databaseURL: "https://noteherder-e7579.firebaseio.com",
+    projectId: "noteherder-e7579",
+    storageBucket: "noteherder-e7579.appspot.com",
+    messagingSenderId: "545881504269"
+}
+firebase.initializeApp(config)
