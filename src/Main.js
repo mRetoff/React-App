0c49772c4d53dfa9ce9508a802d6f8c44cc7b695
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-30 14:28:21 -0400

    Add route that includes note ID

--- src/Main.js
+++ src/Main.js
@@ -1,95 +1,122 @@
-import React from 'react'
-import base from './base'
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 
+import base from './base'
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-class Main extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            currentNote: this.blankNote(),
-            notes: [],
-        }
+class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentNote: this.blankNote(),
+      notes: [],
+    }
+  }
+
+  componentWillMount() {
+    base.syncState(`notes/${this.props.uid}`, {
+      context: this,
+      state: 'notes',
+      asArray: true,
+    })
+  }
+
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
+    }
+  }
+
+  setCurrentNote = (note) => {
+    this.setState({ currentNote: note })
+  }
+
+  resetCurrentNote = () => {
+    this.setCurrentNote(this.blankNote())
+  }
+
+  saveNote = (note) => {
+    const notes = [...this.state.notes]
+
+    if (note.id) {
+      // existing note
+      const i = notes.findIndex(currentNote => currentNote.id === note.id)
+      notes[i] = note
+    } else {
+      // new note
+      note.id = Date.now()
+      notes.push(note)
     }
 
-    componentWillMount() {
-        base.syncState(`notes/${this.props.uid}`, {
-            context: this,
-            state: 'notes',
-            asArray: true,
-        })
+    this.setState({ notes, currentNote: note })
+  }
+
+  removeCurrentNote = () => {
+    const notes = [...this.state.notes]
+    const i = notes.findIndex(note => note.id === this.state.currentNote.id)
+
+    if (i > -1) {
+      notes.splice(i, 1)
+      this.setState({ notes })
     }
 
-    blankNote = () => {
-        return {
-            id: null,
-            title: '',
-            body: '',
-        }
+    this.resetCurrentNote()
+  }
+
+  render() {
+    const formProps = {
+      currentNote: this.state.currentNote,
+      saveNote: this.saveNote,
+      removeCurrentNote: this.removeCurrentNote,
     }
 
-    setCurrentNote = (note) => {
-        this.setState({ currentNote: note })
-    }
+    return (
+      <div
+        className="Main"
+        style={style}
+      >
+        <Sidebar
+          resetCurrentNote={this.resetCurrentNote}
+          signOut={this.props.signOut}
+        />
+        <NoteList
+          notes={this.state.notes}
+          setCurrentNote={this.setCurrentNote}
+        />
+        <Switch>
+          <Route
+            path="/notes/:id"
+            render={navProps => (
+              <NoteForm
+                {...formProps}
+                {...navProps}
+              />
+            )}
+          />
+          <Route
+            render={navProps => (
+              <NoteForm
+                {...formProps}
+                {...navProps}
+              />
+            )}
+          />
+        </Switch>
 
-    resetCurrentNote = () => {
-        this.setCurrentNote(this.blankNote())
-    }
 
-    saveNote = (note) => {
-        const notes = [...this.state.notes]
-
-        if (note.id) {
-            const i = notes.findIndex(currentNote => currentNote.id === note.id)
-            notes[i] = note
-        } else {
-            note.id = Date.now()
-            notes.push(note)
-        }
-
-        this.setState({ notes, currentNote: note })
-    }
-
-    deleteNote = () => {
-        const notes = [...this.state.notes]
-        const i = notes.findIndex(note => note.id === this.state.currentNote.id)
-        if(i > -1) {
-            notes.splice(i, 1)
-            this.setState({ notes })
-        }
-        this.resetCurrentNote()
-    }
-
-    render() {
-        return (
-            <div 
-                className="Main"
-                style={style}
-            >
-                <Sidebar 
-                    resetCurrentNote={this.resetCurrentNote}
-                    signOut={this.props.signOut}
-                />
-                <NoteList 
-                    notes={this.state.notes}
-                    setCurrentNote={this.setCurrentNote}    
-                />
-                <NoteForm 
-                    currentNote={this.state.currentNote}
-                    saveNote={this.saveNote}
-                    deleteNote={this.deleteNote}
-                />
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 const style = {
-    display: 'flex',
-    height: '100vh',
-    alignItems: 'stretch',
+  display: 'flex',
+  height: '100vh',
+  alignItems: 'stretch',
 }
 
 export default Main
