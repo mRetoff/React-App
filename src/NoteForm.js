17634cfb966677a31a84d43f7e858c13b407a3b5
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-30 14:57:46 -0400

    Populate the form based on the route param

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+
 import './NoteForm.css'
 
 class NoteForm extends Component {
@@ -6,6 +7,20 @@ class NoteForm extends Component {
     super(props)
     this.state = {
       note: this.blankNote(),
+    }
+  }
+
+  componentWillReceiveProps = (newProps) => {
+    // Get the ID from the URL
+    const newId = newProps.match.params.id
+
+    // Find the note with that ID
+    const i = newProps.notes.findIndex(currentNote => currentNote.id.toString() === newId.toString())
+    const note = newProps.notes[i]
+
+    // Update state with that note
+    if (note) {
+      this.setState({ note })
     }
   }
 
@@ -26,34 +41,35 @@ class NoteForm extends Component {
 
   render() {
     const { removeCurrentNote } = this.props
+
     return (
-        <div className="NoteForm">
-          <div className="form-actions">
-            <button 
-              type="button"
-              onClick={removeCurrentNote}  
-            >
-              <i className="far fa-trash-alt"></i>
-            </button>
-          </div>
-          <form>
-            <p>
-              <input
-                type="text"
-                name="title"
-                placeholder="Title your note"
-                value={this.state.note.title}
-                onChange={this.handleChanges}
-              />
-            </p>
-            <textarea 
-              name="body" 
-              value={this.state.note.body}
-              onChange={this.handleChanges}
-            >
-            </textarea>
-          </form>
+      <div className="NoteForm">
+        <div className="form-actions">
+          <button
+            type="button"
+            onClick={removeCurrentNote}
+          >
+            <i className="far fa-trash-alt"></i>
+          </button>
         </div>
+        <form>
+          <p>
+            <input
+              type="text"
+              name="title"
+              placeholder="Title your note"
+              value={this.state.note.title}
+              onChange={this.handleChanges}
+            />
+          </p>
+
+          <textarea
+            name="body"
+            value={this.state.note.body}
+            onChange={this.handleChanges}
+          ></textarea>
+        </form>
+      </div>
     )
   }
 }
